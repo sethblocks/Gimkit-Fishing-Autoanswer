75c7fe8cd84a051d575ae6979bbf4db39da79264
sethblocks <56612770+sethblocks@users.noreply.github.com>
2024-01-24 17:07:42 -0800

    Add files via upload

---
+++ gimkit.py
@@ -0,0 +1,76 @@
+from typing import Any
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import io
+
+gimkit = webdriver.Chrome()
+
+#this section creates a list of questions, each value is [question, answer]
+questions = []
+qs = open("questions.txt")
+text = qs.read()
+#print(qs.read())
+q = []
+buf= io.StringIO(text)
+for ln in range(text.count("\n") +1):
+    
+    line = buf.readline()
+    q.append(line.removesuffix('\n'))
+    if len(q) == 2:
+        questions.append(q)
+        q = []
+
+def find_answer(questions: list, question):
+    for qa in questions:
+        if qa[0].lower() == question.lower():
+            #print(qa[0])
+            #print(qa[1])
+            return qa[1]
+#qnum = 11
+#print(questions[qnum][0])
+#print(find_answer(questions, "An example of Leonardo Da Vinci's engineering ideas"))
+
+#print(find_answer(questions, "In his notebooks, Da Vinci sketched about things having to do with flying, such as:"))
+#start gimkit game
+gimkit.get("https://www.gimkit.com/join/6463c7df042b6c00384d783c?authuser=0")
+nameBox = gimkit.find_element(By.CLASS_NAME, "sc-iQAVnG")
+nameBox.send_keys("Seth")
+join = gimkit.find_element(By.CLASS_NAME, "sc-eYulFz")
+join.click()
+try:
+    start = gimkit.find_element(By.CLASS_NAME, "sc-gLgtkKs")
+    start.click()
+except:
+    print("button not found please manually start gimkit")
+input("press enter when ready to answer")
+bait = 0
+while 1==1:
+    try:
+
+        question = gimkit.find_element(By.CLASS_NAME, "sc-qVkRw").text
+        print(question)
+        answer =find_answer(questions, question)
+        if answer != None:
+            buttons = gimkit.find_elements(By.CLASS_NAME, "sc-qVkRw")
+            for button in buttons:
+                if button.text ==answer:
+                    button.click()
+                    bait +=1
+        else:
+            continuebtn = gimkit.find_elements(By.CLASS_NAME, "sc-qVkRw")
+            
+            for btn in continuebtn:
+                if btn.text == "Continue":
+                    btn.click()
+        
+    except:
+        #print("question not found")
+        try:
+            #print(gimkit.find_elements(By.CLASS_NAME, "sc-sImlw").text)
+            if gimkit.find_element(By.CLASS_NAME, "sc-sImlw").text != "Out of bait!":
+                gimkit.find_elements(By.CLASS_NAME, "css-1k9m51z")[0].click()
+        except:
+            pass
+#sc-qVkRw class name for answer buttons
+
+input()
